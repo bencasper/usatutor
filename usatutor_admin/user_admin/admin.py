9dbd67495508037ffbb6e27233f6f02d4e6e10c5
张鹏 <peng.zhang06@luckincoffee.com>
2020-06-23 21:23:22 +0800

    del redundant content

--- usatutor_admin/user_admin/admin.py
+++ usatutor_admin/user_admin/admin.py
@@ -1,37 +1,8 @@
-from django.conf import settings
 from django.contrib import admin
-from django import forms
-from django.template.response import TemplateResponse
+
 from .models import UserTutorResume
 
 
 @admin.register(UserTutorResume)
 class userScheduleAdmin(admin.ModelAdmin):
-    add_form_template = "user_admin/admin/my_schedule.html"
-
-    @property
-    def media(self):
-        js = [
-            'moment.min.js',
-            'jquery.min.js',
-            'jquery-ui.min.js',
-            'fullcalendar.min.js',
-            'scheduler.min.js',
-        ]
-        css = [
-            'fullcalendar.min.css',
-            'scheduler.min.css',
-        ]
-        return forms.Media(css={'all': ['useradmin/css/%s' % url for url in css]},
-                           js=['useradmin/js/%s' % url for url in js])
-
-    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
-        context.update({
-
-        })
-        form_template = self.add_form_template
-        request.current_app = self.admin_site.name
-
-        return TemplateResponse(request, form_template, context)
-
     pass
