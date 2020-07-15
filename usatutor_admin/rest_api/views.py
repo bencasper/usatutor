d9fab161ae46db1d3de7ed338705a311caf56304
张鹏 <peng.zhang06@luckincoffee.com>
2020-07-15 23:25:27 +0800

    排课参数

--- usatutor_admin/rest_api/views.py
+++ usatutor_admin/rest_api/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+import coreapi
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +8,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.filters import BaseFilterBackend
 
 from rest_api.serializer import *
 
@@ -27,13 +31,38 @@ class TutorViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [permissions.AllowAny]
 
 
+class ScheduleFilterBackend(BaseFilterBackend):
+
+    def filter_queryset(self, request, queryset, view):
+        return queryset
+
+    def get_schema_fields(self, view):
+        return [coreapi.Field(
+            name='tutor_id',
+            location='query',
+            required=True,
+            type='string'
+        ), ]
+
+
 class TutorScheduleViewSet(viewsets.ReadOnlyModelViewSet):
     """
     tutor available schedule
+    :parameter === tutor_id
+    return 7天内排课
     """
+    filter_backends = (ScheduleFilterBackend, )
     queryset = UserTutorSchedule.objects.all()
     serializer_class = ScheduleSerializer
     permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        tutor_id = self.request.query_params.get('tutor_id')
+        today = datetime.utcnow().date()
+        start = datetime(today.year, today.month, today.day)
+        util = start + timedelta(8)
+        return UserTutorSchedule.objects.filter(tutor_id=int(tutor_id))\
+            .filter(start_at__gte=today).filter(start_at__lt=util)
 
 
 class MyClassViewSet(viewsets.ModelViewSet):
